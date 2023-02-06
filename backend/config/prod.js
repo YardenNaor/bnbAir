58981ff30d5b44ae48723cd9bde64a531dcc8b77
Yarden Naor <naoryarden@gmail.com>
2023-02-06 21:28:13 +0200

    frying to fix bug eith render

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName : 'stayDB'
+  dbURL: 'mongodb+srv://bnbAir:bnbAir2023@cluster0.yjtgvny.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'stay_DB_class'
 }
